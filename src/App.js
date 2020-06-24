2177633b7a5ced2e3c63102c1378e0126638aaae
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-06-24 14:29:14 -0400

    getting user data from api and populated in the table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
- import SearchForm from "./components/SearchForm";
-// import Main from "./components/Main";
+//  import SearchForm from "./components/SearchForm";
+ import Main from "./components/Main";
 
 function App() {
   return (
     <div>
       <Header />
-      <SearchForm/>
-      {/* <Main /> */}
+      {/* <SearchForm/> */}
+      <Main />
     </div>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import API from "../../utils/API";
 import './style.css'
 import SearchForm from "../../components/SearchForm";
-// import Table from "../../components/Table";
-// import Container from "../../components/Container"
+import Table from "../../components/Table";
+import Container from "../../components/Container"
 
 
 class Main extends Component {
@@ -37,6 +37,34 @@ render() {
           handleInputChange={this.handleInputChange}
           search={this.state.search}
         />
+         <Container>
+        <table className=" col-10 text-center">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name  </th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>City,State,Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {  this.state.results && this.state.results.map((item) =>
+               (
+                <Table
+                  key={item.login.uuid}
+                  image={item.picture.thumbnail}
+                  name={`${item.name.first} ${item.name.last}`}
+                  number={item.cell}
+                  email={item.email}
+                  location={`${item.location.city} , ${item.location.state} , ${item.nat}`}
+                  
+                />
+              ) 
+            )}
+          </tbody>                                                  
+        </table>
+        </Container>
         
       </div>
     );
