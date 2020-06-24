25d9314e2bd6f9286ec502c52d3c8eaf25533cd4
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-06-24 17:33:14 -0400

    added styles for Header

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Container(props) {
-    return (<div className="container mt-5">{props.children}</div>);
+  return <div className="container mt-5">{props.children}</div>;
 }
 
-export default Container
+export default Container;
